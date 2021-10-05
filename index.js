22eb627c5ac5bfb7c100a9bf8678b34456f8cb71
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2021-10-05 18:03:04 -0400

    All tests passed. gonna take a swing at the stretch

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ Do the following:
 
    HINT: no function required
 */
-
+var votingAge = 18;
+if(votingAge >= 18) {
+  console.log(true);
+} 
 
 
 /*
@@ -33,7 +36,11 @@ Do the following:
    HINT: no function required
 */
 
-
+let varOne = 17;
+let varTwo = 17;
+if (varOne === varTwo) {
+  varOne++;
+}
 
 
 
@@ -48,7 +55,9 @@ Do the following:
    HINT: look up the Number method
 */
 
-
+let varStr = '1999';
+let varNum = Number.varStr;
+console.log(varNum);
 
 
 /*
@@ -60,8 +69,8 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(/*add your code here*/){
-  /*add your code here*/
+function multiply(a, b){
+  return a * b;
 }
 
 
@@ -76,8 +85,8 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
+function dogYears(age){
+  return age * 7;
 }
 
 
@@ -150,10 +159,36 @@ RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors
 HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
 
-function game(user, computer){
-  /*add your code here*/
-}
+let computer = Math.floor(Math.random() * 3);
 
+function game(user, computer){
+  // If computer choice is 0 computerChoice = 'rock'
+  // If computer choice is 1 computerChoice = 'paper'
+  // If computer choice is 2 computerChoice = 'scissors'
+  switch(computer) {
+    case 0:
+      computer = 'rock';
+      break;
+    case 1:
+      computer = 'paper';
+      break;
+    case 2:
+      computer = 'scissors';
+      break;
+  }
+  if(user === computer) {
+    return 'it\'s a tie';
+  } else if(user === 'scissors' && computer === 'paper') {
+    return 'you win!';
+  } else if(user === 'paper' && computer === 'rock') {
+    return 'you win!';
+  } else if(user === 'rock' && computer === 'scissors') {
+    return 'you win!';
+  } else {
+    return 'you lose!'
+  }
+}
+console.log(game('paper', computer));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -167,10 +202,9 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
-}
-
+function miles(num){
+  return num * .621371;
+};
 
 
 //Task 5b - Feet to CM
@@ -181,9 +215,9 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
-}
+function feet(cm){
+  return cm / 30.48;
+};
 
 
 
@@ -196,10 +230,11 @@ Using the annoyingSong function below do the following:
 2. At each invocation, it should RETURN this string (note: the tests are expecting the same string as below):
     "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
-
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(num){
+  let numLeftOver = num - 1;
+  return  `${num} bottles of soda on the wall, ${num} bottles of soda, take one down pass it around ${numLeftOver} bottles of soda on the wall`;
 }
+console.log(annoyingSong(100));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -217,8 +252,18 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+  if(score >= 90 && score <= 100) {
+    return 'you got an A';
+  } else if(score >= 80 && score <= 89) {
+    return 'you got a B';
+  } else if(score >= 70 && score <= 79) {
+    return 'you got a C';
+  } else if(score >= 60 && score <= 69) {
+    return 'you got a D';
+  } else {
+    return 'you got an F';
+  }
 }
 
 
